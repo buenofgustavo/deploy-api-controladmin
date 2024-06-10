28c392f1a8194af79679a9af669a7f6120bd55fd
buenofgustavo <156470554+buenofgustavo@users.noreply.github.com>
2024-06-10 16:42:25 -0300

    Update ChatSolicitacoesColaboradoresRepository.java

--- src/main/java/com/centralti/tdm/domain/usuarios/repositories/ChatSolicitacoesColaboradoresRepository.java
+++ src/main/java/com/centralti/tdm/domain/usuarios/repositories/ChatSolicitacoesColaboradoresRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ChatSolicitacoesColaboradoresRepository extends JpaRepository<ChatSolicitacoesColaboradores, String> {
+public interface ChatSolicitacoesColaboradoresRepository extends JpaRepository<ChatSolicitacoesColaboradores, Integer> {
 
     List<ChatSolicitacoesColaboradores> findByIdVinculado(Integer id_vinculado);
 
